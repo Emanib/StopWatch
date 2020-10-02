ca967ed3c6e3bfba57e111716ccbb0c57c2bb54c
Eman Ib <adla48770@gmail.com>
2020-10-02 20:07:05 +0300

    finish

--- Watch.js
+++ Watch.js
@@ -1,5 +1,5 @@
 // Stop watch 
-count = 0; var clearTime; 
+/*count = 0; var clearTime; 
 var seconds = 0, minutes = 0, 
 hours = 0; var clearState; 
 var secs, mins, gethours ;
@@ -19,7 +19,6 @@ var secs, mins, gethours ;
     if ( seconds === 0 && minutes === 0 && hours === 0 ) { 
         var runtime = document.getElementById( "runtime" ); 
         runtime.style.display = "none"; 
-    
        
     }
     }
@@ -46,5 +45,66 @@ var secs, mins, gethours ;
                         }
                             var stop = document.getElementById ("stop");
                            stop.addEventListener( 'click', stopTime);
-                               
-                                 
+                           
+      
+                           function pad (n) {
+                            return ('00' + n).substr(-2);
+                        }
+                        
+     let currenttimer;                             
+function resettimer()
+{
+    stopTime();
+    currenttimer =0; 
+
+
+    
+} */ 
+
+var time = 0;
+var running = 0;
+let start = document.getElementById("start");
+let reset =document.getElementById("reset");
+start.addEventListener("click",startPause);
+reset.addEventListener("click",reset1);
+
+function startPause(){
+	if(running == 0){
+		running = 1;
+		increment();
+		document.getElementById("start").textContent = "Pause";
+	}else{
+		running = 0;
+		document.getElementById("start").textContent = "Resume";
+	}
+};
+
+function reset1(){
+	running = 0;
+	time = 0;
+	document.getElementById("time").textContent = "00:00:00";
+	document.getElementById("start").textContent = "Start";
+};
+
+function increment(){
+	if(running == 1){
+		setTimeout(function(){
+			time++;
+            var mins = Math.floor(time / 10 / 60);
+            console.log(mins);
+			if(mins <= 9){
+				mins = "0" + mins;
+			}
+			var secs = Math.floor(time / 10);
+			if(secs <= 9){
+				secs = "0" + secs;
+			}
+			var tenths = Math.floor(time % 10);
+			if(tenths <= 9){
+				tenths = "0" + tenths;
+			}
+			document.getElementById("time").textContent = mins + ":" + secs + ":" + tenths;
+			increment();
+		}, 100);
+	}
+};
